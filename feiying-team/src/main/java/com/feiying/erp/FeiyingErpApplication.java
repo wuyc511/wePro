9980606195f9c403b67e6aa74ed346a4f98ef98f
wuyc511 <wuyc511@foxmail.com>
2019-08-31 09:58:03 +0800

    commit

--- feiying-team/src/main/java/com/feiying/erp/FeiyingErpApplication.java
+++ feiying-team/src/main/java/com/feiying/erp/FeiyingErpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+//Spring Boot启动类
 @SpringBootApplication
 @MapperScan(basePackages = {"com.feiying.erp.datasource.mappers"})
 @ServletComponentScan
